38f9928158a068d1ee8fe17f05d12ff7bd1b14ea
rupam0011 <rupam001majumder@gmail.com>
2025-03-01 12:16:31 +0530

    Initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useState } from 'react'
+import { assets } from '../../assets/assets'
+import './Sidebar.css'
+import { Context } from '../../context/Context'
+const Sidebar = () => {
+
+  const [extended, setExtended] = useState(false)
+  const { onSent, prevPrompt, setRecentPrompt, newChat } = useContext(Context)
+
+  const loadPrompt = async (prompt) =>{
+    console.log("item",prompt);
+    
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
+
+  return (
+    <div className={`sidebar ${extended ? "extended" : ""}`}>
+
+      <div className="top">
+        <img onClick={() => setExtended(prev => !prev)} className='menu' src={assets.menu_icon} alt="" />
+        <div onClick={()=> newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {
+          extended ?
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              {prevPrompt.map((item, index) => {
+                
+                return (
+                  
+                  <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p key={index}>{item.slice(0,12)}...</p>
+                  </div>
+                )
+              })}
+
+            </div>
+            : null
+        }
+
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Setings</p> : null}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyAFX5NKT3XJ-Hf6FdvQ2SdJPl9OhHn_e1w";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+ export default run;
